97b37d5128f2f444d99bb00d7aa367cbdc549b65
adrianamah <41088860+adrianamah@users.noreply.github.com>
2018-07-10 15:26:09 -0700

    Update my-Java.js

--- my-Java.js
+++ my-Java.js
@@ -1,1 +1,24 @@
+document.addEventListener("DOMContentLoaded",
+function(){
+
+    alert("Hello World!");
+    let myphoto=document.getElementById("chellaphoto")
+
+    myphoto.addEventListener("click", changeImage)
+
+
+    function changeImage(){
+        if (myphoto.src=="file:///Users/girlswhocode2018/Desktop/IMG_8812.JPG"){
+            myphoto.src="IMG_8704.jpg"
+            }
+            else{
+                myphoto.src="firstChella.JPG"
+            }
+
+
+        console.log("heyy");2
+    }
+
+
+}) //end of DOM content DOM Content Loaded
 
